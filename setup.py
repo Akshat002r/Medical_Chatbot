bc3812a652b3423fc249f5ef086c8784fa0efb76
Akshat Srivastava <srivastavaakshat259@gmail.com>
2026-07-03 02:24:14 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical Chatbot",
+    version="0.1.0",
+    author="Akshat",
+    author_email="",
+    packages=find_packages(),
+    install_requires=[]
+)
